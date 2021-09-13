555210a9a35de8eeeecb9f3e781f8dc8c8195e76
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-09-13 14:22:49 -0700

    added wildcard sendFile to server.js, updated submitDiagnostics function, created GET & POST routes in diagnostics

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,25 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a error logging
 diagnostics.post('/', (req, res) => {
-  // TODO: Logic for appending data to the db/diagnostics.json file
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newDiagnostic = {
+      username,
+      tip,
+      topic,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newDiagnostic, './db/diagnostics.json');
+    res.json(`Diagnostic added successfully 🚀`);
+  } else {
+    res.error('Error in adding diagnostic');
+  }
 });
 
 module.exports = diagnostics;
